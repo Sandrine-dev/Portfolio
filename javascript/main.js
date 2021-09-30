2b6332aa976e5c0894e61f8a7357fbf686d2b1bc
Sandrine-dev <sandrine.volpi@gmail.com>
2021-09-30 18:26:25 +0200

    refonte de tous le site

--- javascript/main.js
+++ javascript/main.js
@@ -1,42 +1,130 @@
-/*Collapsible*/
-$(document).on("click", ".collapsible", function() {
-    $(this).next().toggle("slow");
-});
+//Navigation
+    function myFunction() {
+        var nav = document.getElementById("MyTopnav");
+        if (nav.className === "topnav") {
+        nav.className += " responsive";
+        } else {
+        nav.className = "topnav";
+        }
+    };
 
-/*dot animation*/
+    window.onscroll = function() {fixed()};
+    var navbar = document.getElementById("MyTopnav");
+    var sticky = navbar.offsetTop;
 
-var dots = 0;
+    function fixed(){
+        if( window.pageYOffset >= sticky) {
+            navbar.classList.add("fixNavigation");
+        } else {
+            navbar.classList.remove("fixNavigation");
+        }
+    };
 
-$(document).ready(function() {
-    setInterval (type, 600);
-});
+//Skills typing
 
-function type() {
-    if (dots < 3) {
-        $(".dots").append('.');
-        dots++;
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
     } else {
-        $(".dots").html('');
-        dots = 0;
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
     }
-}
 
-/* Button Top*/
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
-mybutton = document.getElementById("MyBtn");
+    var that = this;
+    var delta = 200 - Math.random() * 100;
 
-window.onscroll = function() {scrollFunction()};
+    if (this.isDeleting) { delta /= 2; }
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #00BCD4}";
+    document.body.appendChild(css);
+};
+
+//Sites 
+
+filterSelection("all") // Execute the function and show all columns
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("column");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
   }
 }
 
+// Show filtered elements
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
 
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
 }
