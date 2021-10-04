59122ee9b7cd7446ded18f694dc05caa94a40cc6
Sandrine-dev <sandrine.volpi@gmail.com>
2021-10-04 19:44:43 +0200

    ajout langue et animation

--- javascript/main.js
+++ javascript/main.js
@@ -20,6 +20,33 @@
         }
     };
 
+//Animation
+const ratio = .3;
+
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: ratio
+};
+
+const handleIntersect = function(entries, observer){
+  entries.forEach(function (entry){
+    if(entry.intersectionRatio > ratio){
+      console.log('visible');
+      entry.target.classList.add('reveal-visible');
+      observer.unobserve(entry.target);
+    } else{
+      //console.log('invisible');
+    }
+    
+  });
+};
+
+const observer = new IntersectionObserver(handleIntersect, options);
+document.querySelectorAll('.reveal').forEach(function(r){
+  observer.observe(r);
+})
+
 //Skills typing
 
 var TxtType = function(el, toRotate, period) {
